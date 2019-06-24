17b12e0266ba4ac0b725757a1d946c125a4ae367
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-06-24 17:18:50 -0300

    object short syntax

---
+++ es6/concepts/object_short_syntax.js
@@ -0,0 +1,16 @@
+const nome = 'Patrick';
+const idade = 29;
+
+const usuario = {
+  nome: nome,
+  idade: idade,
+  empresa: 'Godigo'
+};
+console.log(usuario);
+
+const usuario2 = {
+  nome,
+  idade,
+  empresa: 'Godigo'
+};
+console.log(usuario2);
