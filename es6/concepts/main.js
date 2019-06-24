7e05075b5fdb3edb4e81a973cae5b1340c91dc35
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-06-24 11:53:46 -0300

    merging all concepts lessons

--- es6/concepts/main.js
+++ es6/concepts/main.js
@@ -1,3 +1,32 @@
+class List {
+  constructor() {
+    this.data = [];
+  }
+
+  add(data) {
+    this.data.push(data);
+    console.log(this.data);
+  }
+}
+
+class TodoList extends List {
+  constructor() {
+    super();
+    this.usuario = 'Patrick';
+  }
+
+  mostraUsuario() {
+    console.log(this.usuario);
+  }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+  MinhaLista.add('Uea');
+}
+MinhaLista.mostraUsuario();
+
 class Matematica {
   static soma(a, b) {
     return a + b;
