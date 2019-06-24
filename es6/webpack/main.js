82725e046cb3839f8d31766b7d796841cb8616d0
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-06-24 17:45:56 -0300

    import and export

--- es6/webpack/main.js
+++ es6/webpack/main.js
@@ -1,5 +1,13 @@
 // carregar todos arquivos através do webpack
 // código ainda vai ser transpilado para o bundle com o babel
-import { soma } from './funcoes';
+import * as funcoes from './funcoes';
+// import soma, { sub, mult } from './funcoes';
+// import { soma as someFunction, sub } from './funcoes';
+// import soma from './soma';
 
-console.log(soma(1, 2));
+// console.log(someFunction(1, 2));
+// console.log(soma(1, 2));
+// console.log(sub(5, 2));
+console.log(funcoes.default(1, 2));
+console.log(funcoes.sub(5, 2));
+console.log(funcoes.mult(5, 2));
