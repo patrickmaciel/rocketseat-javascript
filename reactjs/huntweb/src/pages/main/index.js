cd7c63e45eb29da8e85b4d6e54c90e0ccd281867
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-06-25 17:08:00 -0300

    set values in state

--- reactjs/huntweb/src/pages/main/index.js
+++ reactjs/huntweb/src/pages/main/index.js
@@ -4,6 +4,10 @@ import React, {
 import api from '../../services/api';
 
 export default class Main extends Component {
+  state = {
+    products: [],
+  }
+
   componentDidMount() {
     // assim que o componente for mostrado em tela
     this.loadProducts();
@@ -12,9 +16,17 @@ export default class Main extends Component {
   loadProducts = async () => {
     const response = await api.get('products');
     // response.data.docs
+    this.setState({ products: response.data.docs });
   };
 
   render() {
-    return <h1> Hello Rocketseat </h1>;
+    // return <h1> Hello Rocketseat {this.state.products.length} </h1>;
+    return (
+      <div className="product-list">
+        {this.state.products.map(product => (
+          <h2 key={product._id}>{product.title}</h2>
+        ))}
+      </div>
+    )
   }
 }
