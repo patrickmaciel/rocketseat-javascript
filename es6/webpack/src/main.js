f9ebfd4391e9d825f2ba0dc0bd48d8074247be03
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-06-25 13:53:58 -0300

    searching in github api using axios

--- es6/webpack/src/main.js
+++ es6/webpack/src/main.js
@@ -1,7 +1,11 @@
+import api from './api';
+
 class App {
   constructor() {
     this.repositories = [];
     this.formEl = document.getElementById('repo-form');
+    this.inputEl = document.querySelector('input[name=repository]');
+    this.listElement = document.getElementById('repo-list');
     this.registerHandlers();
   }
 
@@ -12,15 +16,56 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
-  addRepository(event) {
+  async addRepository(event) {
     event.preventDefault();
+
+    const repoInput = this.inputEl.value;
+    if (repoInput.length === 0) {
+      return;
+    }
+
+    const response = await api.get(`/repos/${repoInput}`);
+
+    const { name, description, html_url, owner: { avatar_url }} = response.data;
+
     this.repositories.push({
-      name: 'Patrick Maciel',
-      description: 'Engenheiro de Software na Recrutei',
-      avatar_url: 'https://avatars3.githubusercontent.com/u/671670?s=460&v=4',
-      html_url: 'https://github.com/patrickmaciel'
+      name,
+      description,
+      avatar_url,
+      html_url
     });
-    console.log(this.repositories);
+
+    this.inputEl.value = '';
+
+    this.render();
+  }
+
+  render() {
+    this.listElement.innerHTML = '';
+    // this.repositories.map
+    this.repositories.forEach(repo => {
+      let imgEl = document.createElement('img');
+      imgEl.setAttribute('src', repo.avatar_url);
+
+      let titleEl = document.createElement('strong');
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement('p');
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement('a');
+      linkEl.setAttribute('href', repo.html_url);
+      linkEl.setAttribute('target', '_blank');
+      linkEl.appendChild(document.createTextNode('Acessar'));
+
+      let listItemEl = document.createElement('li');
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listElement.appendChild(listItemEl);
+    });
   }
 }
 
