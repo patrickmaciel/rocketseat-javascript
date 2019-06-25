2d834a16e649e7d6b36ee928852f6b23ed082f35
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-06-25 17:27:17 -0300

    next and prev page for pagination navigation

--- reactjs/huntweb/src/pages/main/index.js
+++ reactjs/huntweb/src/pages/main/index.js
@@ -8,6 +8,8 @@ import './styles.css';
 export default class Main extends Component {
   state = {
     products: [],
+    productInfo: {},
+    page: 1,
   }
 
   componentDidMount() {
@@ -15,14 +17,31 @@ export default class Main extends Component {
     this.loadProducts();
   }
 
-  loadProducts = async () => {
-    const response = await api.get('products');
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`products?page=${page}`);
     // response.data.docs
-    this.setState({ products: response.data.docs });
+    const { docs, ...productInfo } = response.data;
+    this.setState({ products: docs, productInfo, page });
   };
 
+  nextPage = () => {
+    const { page, productInfo } = this.state;
+    if (page === productInfo.pages) return;
+
+    const pageNumber = page + 1;
+    this.loadProducts(pageNumber);
+  }
+
+  prevPage = () => {
+    const { page, productInfo } = this.state;
+    if (page === 1) return;
+
+    const pageNumber = page - 1;
+    this.loadProducts(pageNumber);
+  }
+
   render() {
-    const { products } = this.state;
+    const { products, page, productInfo } = this.state;
     // return <h1> Hello Rocketseat {this.state.products.length} </h1>;
     return (
       <div className="product-list">
@@ -33,7 +52,12 @@ export default class Main extends Component {
             <a href="#">Acessar</a>
           </article>
         ))}
+
+        <div className="actions">
+          <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+          <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próxima</button>
+        </div>
       </div>
-    )
+    );
   }
 }
