78ed090451941195f4dc04971a3e21df239daab8
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-06-25 14:39:53 -0300

    connect in mongodb using mongoose

--- nodejs/node-api/server.js
+++ nodejs/node-api/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// start app
 const app = express();
 
+// start mongodb database
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+
+// first route
 app.get('/', (req, res) => {
-  res.send('Olopa á');
+  res.send('Estudar é bom');
 });
 
 app.listen(3001);
