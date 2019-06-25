ea174b5e90a41254ffb983ade02e6905ed334b71
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-06-25 15:33:25 -0300

    structuring files and directories

--- nodejs/node-api/server.js
+++ nodejs/node-api/server.js
@@ -10,17 +10,7 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true })
 
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-// first route
-app.get('/', (req, res) => {
-  Product.create({
-    title: 'React',
-    description: 'Build native apps',
-    url: 'https://github.com/facebook/react-native'
-  });
-
-  return res.send('Estudar é bom');
-});
+// router
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
