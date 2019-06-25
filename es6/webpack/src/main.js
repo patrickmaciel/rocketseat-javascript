25897bef8678369e8314df76abdddb039e7a5c43
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-06-25 10:54:06 -0300

    async wait

--- es6/webpack/src/main.js
+++ es6/webpack/src/main.js
@@ -1,1 +1,35 @@
 console.log('webpack-dev-server on');
+
+// async await content
+const minhaPromise = () => new Promise((resolve, reject) => {
+  setTimeout(() => { resolve('OK') }, 2000);
+});
+
+// minhaPromise()
+//   .then(response => {
+//     console.log(response);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+// async function executaPromise() {
+//   console.log(await minhaPromise());
+//   console.log(await minhaPromise());
+//   console.log(await minhaPromise());
+//   console.log(await minhaPromise());
+//   console.log(await minhaPromise());
+//   console.log(await minhaPromise());
+// };
+
+// executaPromise();
+
+const executaPromise = async () => {
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+}
+executaPromise();
