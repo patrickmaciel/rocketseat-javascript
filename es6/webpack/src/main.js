5ff1d30f5d2e2ce3be52eb681f4c84d6e08b4f53
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-06-25 13:59:06 -0300

    loading message

--- es6/webpack/src/main.js
+++ es6/webpack/src/main.js
@@ -16,6 +16,18 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
+  setLoading(loading = true) {
+    if (loading) {
+      let loadingEl = document.createElement('span');
+      loadingEl.appendChild(document.createTextNode('Carregando...'));
+      loadingEl.setAttribute('id', 'loading');
+
+      this.formEl.appendChild(loadingEl);
+    } else {
+      document.getElementById('loading').remove();
+    }
+  }
+
   async addRepository(event) {
     event.preventDefault();
 
@@ -24,20 +36,27 @@ class App {
       return;
     }
 
-    const response = await api.get(`/repos/${repoInput}`);
+    this.setLoading();
+    try {
+      const response = await api.get(`/repos/${repoInput}`);
 
-    const { name, description, html_url, owner: { avatar_url }} = response.data;
+      const { name, description, html_url, owner: { avatar_url }} = response.data;
 
-    this.repositories.push({
-      name,
-      description,
-      avatar_url,
-      html_url
-    });
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url
+      });
 
-    this.inputEl.value = '';
+      this.inputEl.value = '';
 
-    this.render();
+      this.render();
+    } catch (err) {
+      alert('O Repositório não existe');
+    }
+
+    this.setLoading(false);
   }
 
   render() {
