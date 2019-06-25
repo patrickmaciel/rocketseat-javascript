d5c0e587eef4ef91c6a240b2bc9fff6735bf53ed
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-06-25 16:48:46 -0300

    creating header component

--- reactjs/huntweb/src/App.js
+++ reactjs/huntweb/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Header from './components/Header'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Patrick</h1>
-    </div>
-  );
-}
+import './styles.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello Patrick</h1>
+//     </div>
+//   );
+// }
+
+const App = () => (
+  <div className='App'>
+    <Header />
+  </div>
+);
 
 export default App;
