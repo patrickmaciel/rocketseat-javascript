276eaa7154345a35ef2b6497a129b53c95f8fc74
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-06-25 16:00:33 -0300

    added crud endpoints

--- nodejs/node-api/src/controllers/ProductController.js
+++ nodejs/node-api/src/controllers/ProductController.js
@@ -11,5 +11,22 @@ module.exports = {
   async store(req, res) {
     const product = await Product.create(req.body);
     return res.json(product);
+  },
+
+  async show(req, res) {
+    const product = await Product.findById(req.params.id)
+    return res.json(product);
+  },
+
+  async update(req, res) {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    return res. json(product);
+  },
+
+  async destroy(req, res) {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    return res.json({
+      message: 'Deleted'
+    });
   }
 }
