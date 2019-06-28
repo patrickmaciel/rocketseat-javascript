e6a430563b27ad7c07859b3afedae6d85e2ef535
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-06-28 11:54:13 -0300

    styling header and statusbar

---
+++ react-native/hunt/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Routes from './routes';
+import './config/StatusBarConfig';
+
+const App = () => <Routes />;
+
+export default App;
+
+/*
+class App extends Component {
+  render() {
+    return <Routes />
+  }
+}
+*/
